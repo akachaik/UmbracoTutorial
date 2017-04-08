d579b45a6f8613ac746d6c45f7153e0339b9db5c
Akachai <AkachaiPC@mail.com>
2017-04-08 18:32:59 +0700

    refactor GetChildNavigationList

--- WebApplication4/Controllers/SiteLayoutController.cs
+++ WebApplication4/Controllers/SiteLayoutController.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+//using System.Linq;
 using System.Runtime.Caching;
 using System.Text;
 using System.Threading.Tasks;
 using System.Web.Mvc;
 using Umbraco.Core.Models;
+using Umbraco.Web;
 using Umbraco.Web.Mvc;
 using WebApplication4.Models;
 
@@ -38,48 +40,64 @@ namespace WebApplication4.Controllers
         //}
         private List<NavigationListItem> GetNavigationModelFromDatabase()
         {
-            const int HOME_PAGE_POSITION_IN_PATH = 1;
-            int homePageId = int.Parse(CurrentPage.Path.Split(',')[HOME_PAGE_POSITION_IN_PATH]);
+            const int homePagePositionInPath = 1;
+            var homePageId = int.Parse(CurrentPage.Path.Split(',')[homePagePositionInPath]);
 
-            IPublishedContent homePage = Umbraco.Content(homePageId);
-            List<NavigationListItem> nav = new List<NavigationListItem>();
-            nav.Add(new NavigationListItem(new NavigationLink(homePage.Url, homePage.Name)));
+            var homePage = Umbraco.Content(homePageId);
+            var nav = new List<NavigationListItem>
+            {
+                new NavigationListItem(new NavigationLink(homePage.Url, homePage.Name))
+            };
             nav.AddRange(GetChildNavigationList(homePage));
             return nav;
         }
 
-        private List<NavigationListItem> GetChildNavigationList(dynamic page)
+        private List<NavigationListItem> GetChildNavigationList(IPublishedContent page)
         {
-            List<NavigationListItem> listItems = null;
             var childPages = page.Children.Where("Visible");
-            if (childPages != null && childPages.Any() && childPages.Count() > 0)
+            if (childPages == null)
             {
-                listItems = new List<NavigationListItem>();
-                foreach (var childPage in childPages)
+                return new List<NavigationListItem>();
+            }
+
+            if (!childPages.Any())
+            {
+                return new List<NavigationListItem>();
+            }
+
+            var result = new List<NavigationListItem>();
+
+            foreach (var childPage in childPages)
+            {
+                var listItem = new NavigationListItem(new NavigationLink(childPage.Url, childPage.Name))
                 {
-                    NavigationListItem listItem = new NavigationListItem(new NavigationLink(childPage.Url, childPage.Name));
-                    listItem.Items = GetChildNavigationList(childPage);
-                    listItems.Add(listItem);
-                }
+                    Items = GetChildNavigationList(childPage)
+                };
+
+                result.Add(listItem);
+
             }
-            else
-            {
-                listItems = new List<NavigationListItem>();
-            }
-            return listItems;
+
+            return result;
         }
 
         private static T GetObjectFromCache<T>(string cacheItemName, int cacheTimeInMinutes, Func<T> objectSettingFunction)
         {
             ObjectCache cache = MemoryCache.Default;
             var cachedObject = (T)cache[cacheItemName];
-            if (cachedObject == null)
+
+            if (cachedObject != null)
             {
-                CacheItemPolicy policy = new CacheItemPolicy();
-                policy.AbsoluteExpiration = DateTimeOffset.Now.AddMinutes(cacheTimeInMinutes);
-                cachedObject = objectSettingFunction();
-                cache.Set(cacheItemName, cachedObject, policy);
+                return cachedObject;
             }
+
+            var policy = new CacheItemPolicy
+            {
+                AbsoluteExpiration = DateTimeOffset.Now.AddMinutes(cacheTimeInMinutes)
+            };
+            cachedObject = objectSettingFunction();
+            cache.Set(cacheItemName, cachedObject, policy);
+
             return cachedObject;
         }
     }
